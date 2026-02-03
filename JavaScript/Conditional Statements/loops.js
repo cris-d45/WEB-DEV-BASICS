4da113c71c9c8368f0d513fafb60dcb1b545772f
“cris-d45” <dcris626@gmail.com>
2026-02-03 09:43:45 -0800

    Adding execise, for loops

---
+++ JavaScript/Conditional Statements/loops.js
@@ -0,0 +1,16 @@
+// for loop
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log("Iteration:", i);
+// }
+// Starts at i = 0
+// Runs WHILE i is still less than or equal to 5 ( <= 5; )
+// The operator increases i by one each time ( i++ ), until it breaks because it cannot reach 6.
+// console.log logs the loops.
+
+// const colors = ["Red", "Green", "Blue", "Yellow"];
+
+// for (let i = 0; i < colors.length; i++) {
+//     console.log("i =", i);
+//     console.log("colors[i] =", colors[i])
+// }
