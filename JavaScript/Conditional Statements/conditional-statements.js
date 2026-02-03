491adcbd20573db217c6a3526e605bb624239fc4
“cris-d45” <dcris626@gmail.com>
2026-02-02 22:36:47 -0800

    adding exercise, conditional statements

---
+++ JavaScript/Conditional Statements/conditional-statements.js
@@ -0,0 +1,23 @@
+// // if statement
+
+// let temperature = 30; // number that is 21
+
+// if (temperature > 25) {
+//   console.log("It's a hot day, stay hydrated!");
+// } // since we're using the comparison operator, if the temperature was lower than 25, the console log would disappear.
+
+//if...else if...else
+
+const marks = 100;
+
+if (marks >= 90) {
+  console.log("Grade: A");
+} else if (marks >= 80) {
+  console.log("Grade: 80");
+} else if (marks >= 70) {
+  console.log("Grade: C");
+} else if (marks >= 60) {
+  console.log("Grade: D");
+} else {
+  console.log("Grade: F");
+}
