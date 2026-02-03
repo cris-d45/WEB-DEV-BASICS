468bc8039eb044e248b56b16a137d1985146fe6a
“cris-d45” <dcris626@gmail.com>
2026-02-03 14:12:32 -0800

    adding exercise, Declaring and calling functions

---
+++ JavaScript/Functions/script.js
@@ -0,0 +1,18 @@
+//Function basics, declaring and calling (Example)
+// function greet() {
+//   console.log("Hello, welcome to javascript");
+// }
+
+// greet(); // calling the function
+
+// function greetUser(name) {
+//   console.log(`Hello, ${name}!`);
+// }
+
+// greetUser("Alice");
+// greetUser("Bob");
+// greetUser("Charles");
+
+
+
+//
