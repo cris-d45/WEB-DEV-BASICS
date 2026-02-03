da2f415fb66e5c9216eea61ebf01ee6ba0bddbcf
“cris-d45” <dcris626@gmail.com>
2026-02-03 14:22:42 -0800

    adding exercise, functions with multiple parameters

--- JavaScript/Functions/script.js
+++ JavaScript/Functions/script.js
@@ -13,6 +13,11 @@
 // greetUser("Bob");
 // greetUser("Charles");
 
+//Functions with Multiple Parameters
+function addNumbers(num1, num2, num3) {
+  console.log(`Sum: ${num1 + num2 + num3}`);
+}
 
-
-//
+addNumbers(1, 2, 3); // Output: 6
+// addNumbers(5, 10); //Output: 15
+// addnumbers(10, 10); //Output: 20
