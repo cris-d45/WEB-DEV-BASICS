2f168238bb71b55e79ddbb3b0612995bba7bd51c
“cris-d45” <dcris626@gmail.com>
2026-02-04 14:11:25 -0800

    adding exercise, array methods pop, push, shift, unshift

--- JavaScript/Arrays and Objects/array.js
+++ JavaScript/Arrays and Objects/array.js
@@ -1,11 +1,25 @@
 const fruits = ["Apple", "Banana", "Cherry"];
 console.log(fruits); // Output: ["Apple", "Banana", "Cherry"]
 
-console.log(fruits[0]);
-console.log(fruits[2]);
+// Declaring and Manipulating Arrays
+// console.log(fruits[0]);
+// console.log(fruits[2]);
 
-fruits[1] = "Blueberry";
+// fruits[1] = "Blueberry";
 
+// console.log(fruits);
+
+// console.log(fruits.length); // Output 3
+
+// Array methods
+fruits.push("Mango");
 console.log(fruits);
 
-console.log(fruits.length);
+fruits.pop(); //This removes the element at the end of the array.
+console.log(fruits);
+
+fruits.shift(); //This removes the element at the beginning of the array.
+console.log(fruits);
+
+fruits.unshift("Grapes"); //This adds an element at the beginning of the array.
+console.log(fruits);
