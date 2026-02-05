a03f6aeaab06d88159985a678121115149a98383
“cris-d45” <dcris626@gmail.com>
2026-02-04 23:23:09 -0800

    adding exercise, declaring and manipulating objects

---
+++ JavaScript/Arrays and Objects/objects.js
@@ -0,0 +1,23 @@
+const person = {
+  //These objects are also called dictionaries in Python
+  name: "Alice",
+  age: 25,
+  city: "New York",
+};
+
+console.log(person);
+
+console.log(person.name);
+// console.log(person.city);
+console.log(person["age"]); // pretty much the same as person.name, just different syntax
+
+person.county = "USA"; // we added .country as a property
+console.log(person);
+
+person.age = 30; // this is updating the age property from 25 to 30.
+console.log(person);
+
+delete person.city; // this is selectively deleting the city property.
+console.log(person);
+
+
