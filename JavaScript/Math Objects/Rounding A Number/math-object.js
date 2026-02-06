86e80b1988c0e11a875e2ad39c7407e2e35abf30
“cris-d45” <dcris626@gmail.com>
2026-02-05 22:47:31 -0800

    adding exercise, using Math.max, .min and abs

--- JavaScript/Math Objects/Rounding A Number/math-object.js
+++ JavaScript/Math Objects/Rounding A Number/math-object.js
@@ -23,3 +23,19 @@ console.log("Math.floor(4.1)", Math.floor(4.1));
 console.log("Math.ceil() --------");
 console.log("Math.ceil(4.1)", Math.ceil(4.1));
 console.log("Math.ceil(4.9)", Math.ceil(4.9));
+
+//Math.max and Math.min find the largest / smallest numner
+console.log("Math.max() --------");
+console.log("Math.max(10, 20, 5, 40, 30)", Math.max(10, 20, 5, 40, 30));
+console.log("Math.min() ------");
+console.log("Math.min(10, 20, 5, 40, 30)", Math.min(10, 20, 5, 40, 30));
+
+const nums = [1, 2, 3, 4, 5, 10, 40, 100, 44, 30];
+console.log("Math.min(...nums)", Math.min(...nums));
+console.log("Math.max(...nums)", Math.max(...nums));
+
+// Math.abs() - Gets the absolute value of a number
+// converts negative numbers to positive
+console.log("Math.abs() ------------");
+console.log("Math.abs(-10)", Math.abs(-10));
+console.log("Math.abs(10)", Math.abs(10));
