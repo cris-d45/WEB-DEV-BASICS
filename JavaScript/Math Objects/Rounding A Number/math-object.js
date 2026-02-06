460dce065dcac6ebbd0e6fdf4f9a4ad49c949410
“cris-d45” <dcris626@gmail.com>
2026-02-05 21:54:48 -0800

    adding exercise, rounding a number, math objects

---
+++ JavaScript/Math Objects/Rounding A Number/math-object.js
@@ -0,0 +1,25 @@
+// console.log(Math.PI);
+// console.log(Math.E);
+
+// // Math.round - rounds a number to the nearest integer
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.3));
+
+// // Math.floor - Rounds down to the nearest integer
+// console.log(Math.floor(4.9));
+// console.log(Math.floor(4.1));
+
+// // Math.round - rounds a number to the nearest integer (Example 2)
+console.log("Math.round() --------");
+console.log("Math.round(4.6)", Math.round(4.6));
+console.log("Math.round(4.3)", Math.round(4.3));
+
+// // Math.floor - Rounds down to the nearest down integer (Example 2)
+console.log("Math.floor() --------");
+console.log("Math.floor(4.9)", Math.floor(4.9));
+console.log("Math.floor(4.1)", Math.floor(4.1));
+
+// Math.Ceil - Rounds up to the nearest integer
+console.log("Math.ceil() --------");
+console.log("Math.ceil(4.1)", Math.ceil(4.1));
+console.log("Math.ceil(4.9)", Math.ceil(4.9));
