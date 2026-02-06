fd7af31576e6b4abc0700f07f84e2f95d0978aab
“cris-d45” <dcris626@gmail.com>
2026-02-06 07:43:57 -0800

    Completed project, dice roller and score tracker

--- JavaScript/Rounding A Number/PROJECT/dice-roller.js
+++ JavaScript/Rounding A Number/PROJECT/dice-roller.js
@@ -1,7 +1,35 @@
-console.log("Hello");
-
-
 // <! -- 🎲 -->
 // <! -- 🎲 -->
 
+const scoreTracker = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+};
 
+// roll dice function get number from 1 - 6
+function rollDice() {
+  const roll = Math.floor(Math.random() * 6) + 1;
+  scoreTracker[roll]++; // increase count of rolled number.
+  console.log(`<🎲 You rolled a ${roll}`);
+}
+
+// rollDice();
+
+function displayScores() {
+  console.log(`<🎲 Dice Roll Score Tracker:`);
+  for (const roll in scoreTracker) {
+    console.log(`${roll}: ${scoreTracker[roll]} times`);
+  }
+}
+
+// // stiumating dice rolls
+for (let i = 0; i < 100; i++) {
+  rollDice();
+}
+
+// display final score summary
+displayScores();
